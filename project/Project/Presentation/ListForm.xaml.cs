19ae1cf0899d276cc869ddf6cdfc17b41e1724d2
kaitohakubane <kaitohakubane@gmail.com>
2015-11-29 21:21:10 +0700

    ss2

--- project/Project/Presentation/ListForm.xaml.cs
+++ project/Project/Presentation/ListForm.xaml.cs
@@ -9,17 +9,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Project
+namespace Project.Presentation
 {
     /// <summary>
-    /// Interaction logic for ListControl.xaml
+    /// Interaction logic for ListForm.xaml
     /// </summary>
-    public partial class ListControl : UserControl
+    public partial class ListForm : Window
     {
-        public ListControl()
+        public ListForm()
         {
             InitializeComponent();
         }
