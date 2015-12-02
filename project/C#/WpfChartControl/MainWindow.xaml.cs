3be1ea460b0097414e47d5ce1e9afa16872251ac
mymywas <mynvtse61526@fpt.edu.vn>
2015-12-02 22:10:04 +0700

    ChartDemo

---
+++ project/C#/WpfChartControl/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfChartControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            showChart();
+        }
+        private void showChart()
+        {
+            List<KeyValuePair<string, int>> MyValue = new List<KeyValuePair<string, int>>();
+            MyValue.Add(new KeyValuePair<string, int>("ngay 1", 20));
+            MyValue.Add(new KeyValuePair<string, int>("ngay 2", 36));
+            MyValue.Add(new KeyValuePair<string, int>("ngay 3", 89));
+            MyValue.Add(new KeyValuePair<string, int>("ngay 4", 270));
+            MyValue.Add(new KeyValuePair<string, int>("ngay 5", 140));
+
+            ColumnChart1.DataContext = MyValue;
+        }
+    }
+}
